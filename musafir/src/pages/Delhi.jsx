53637bcf446f05678b8be8858490ea5809211fea
morevishal99 <morevishal060298@gmail.com>
2023-01-19 11:17:21 +0530

    added hotels data

---
+++ musafir/src/pages/Delhi.jsx
@@ -0,0 +1,65 @@
+import React from 'react'
+
+const Delhi = ({ price, dscprice, offer, freecancellation, location2, name, image, imgThumb1, imgThumb2, imgThumb3, imgThumb4, rating, ratingText, location, item, cancellation, pernight, tax }) => {
+    return (<>
+        <div style={{ width: "90%", margin: "auto", display: "flex", gap: "10px", padding: "10px", margin: "20px" }} >
+
+            <div style={{ width: "20%", margin: "auto" }}>
+                <div>
+                    <img width={"320px"} height={"220px"} src={image} alt="" />
+                </div>
+
+                <div style={{ display: "flex", gap: "10px" }}>
+                    <img width={"100px"} src={imgThumb1} alt="" />
+                    <img width={"100px"} src={imgThumb2} alt="" />
+                    <img width={"100px"} src={imgThumb3} alt="" />
+                </div>
+
+            </div>
+
+
+            <div style={{ width: "70%", margin: "auto", gap: "10px", padding: "10px", margin: "20px", marginLeft: "50px" }}>
+                <div style={{ display: "flex", gap: "10px" }}>
+                    <h5>{rating}</h5>
+                    <h5>{ratingText}</h5>
+
+                </div>
+                <div style={{ display: "flex", gap: "10px" }}>
+                    <h5>{name}</h5>
+
+                </div>
+                <div style={{ display: "flex", gap: "10px" }}>
+                    <h5>{location2}</h5>
+                    <h5>{location}</h5>
+
+                </div>
+                <div style={{ display: "flex", gap: "10px" }}>
+
+                    <h5>{item}</h5>
+
+                </div>
+
+                <div style={{ gap: "10px" }}>
+                    <h5>{cancellation}</h5>
+                    <h5>{freecancellation}</h5>
+                    <h5>{offer}</h5>
+                </div>
+
+
+
+            </div>
+            <div style={{ gap: "10px" }}>
+                <h5>{price}</h5>
+                <h5>{dscprice}</h5>
+                <h5>{tax}</h5>
+                <h5>{pernight}</h5>
+            </div>
+        </div>
+
+    </>
+    )
+}
+
+export default Delhi
+
+
--- musafir/src/pages/Hotel.jsx
+++ musafir/src/pages/Hotel.jsx
@@ -5,6 +5,8 @@ import { Box } from '@mui/system'
 import React, { useState } from 'react'
 import hotels from "../hotel.json"
 import Mumbai from "./Mumbai"
+import Delhi from "./Delhi"
+import Pune from "./Pune"
 const Hotel = () => {
   const [mumbai, setMumbai] = useState(hotels.hotels.mumbai)
   const [pune, setpune] = useState(hotels.hotels.pune)
@@ -14,7 +16,7 @@ const Hotel = () => {
 
   const [data, setdata] = useState([])
   console.log(mumbai)
-  // console.log(pune)
+  console.log(pune)
   // console.log(delhi)
 
   const handleChange = (e) => {
@@ -29,62 +31,107 @@ const Hotel = () => {
 
   const handleclick = () => {
 
-  }   
-//  let    mdata= mumbai.map((e) => <Box key={e.id}> <h1 > {e.grayText}</h1> <h1 > {e.blueText}</h1> <img src={e.img} alt="" /> </Box>) 
-// const mydata= mumbai.map((e) =>{  id=key={e.id} price={e.grayText} text={e.blueText} img={e.img} }  ) 
+  }
+  //  let    mdata= mumbai.map((e) => <Box key={e.id}> <h1 > {e.grayText}</h1> <h1 > {e.blueText}</h1> <img src={e.img} alt="" /> </Box>) 
+  // const mydata= mumbai.map((e) =>{  id=key={e.id} price={e.grayText} text={e.blueText} img={e.img} }  ) 
 
   return (<>
-   
-      <FormControl  >
-        <InputLabel id="demo-simple-select-label">Select City</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={value}
-          label="Select City"
-          onChange={handleChange}
-        >
-          <MenuItem value={"mumbai"}>Mumbai</MenuItem>
-          <MenuItem value={"pune"}>Pune</MenuItem>
-          <MenuItem value={"delhi"}>Delhi</MenuItem>
-        </Select>
-      </FormControl>
-      <form onSubmit={handleSubmit} action="">
-        <TextField margin="dense"
-          type="text"
-          value={value}
-          onChange={handleChange}
-        />
-        <Button onClick={handleclick}>Search</Button>
-      </form>
-   
-     {value === "mumbai" ? mumbai.map((e) => <Box key={e.id}> <h1 > {e.grayText}</h1> <h1 > {e.blueText}</h1> <img src={e.img} alt="" /> </Box>) : ""}
+
+    <FormControl  >
+      <InputLabel id="demo-simple-select-label">Select City</InputLabel>
+      <Select
+        labelId="demo-simple-select-label"
+        id="demo-simple-select"
+        value={value}
+        label="Select City"
+        onChange={handleChange}
+      >
+        <MenuItem value={"mumbai"}>Mumbai</MenuItem>
+        <MenuItem value={"pune"}>Pune</MenuItem>
+        <MenuItem value={"delhi"}>Delhi</MenuItem>
+      </Select>
+    </FormControl>
+    <form onSubmit={handleSubmit} action="">
+      <TextField margin="dense"
+        type="text"
+        value={value}
+        onChange={handleChange}
+      />
+      <Button onClick={handleclick}>Search</Button>
+    </form>
+
+    {/* {value === "mumbai" ? mumbai.map((e) => <Box key={e.id}> <h1 > {e.grayText}</h1> <h1 > {e.blueText}</h1> <img src={e.img} alt="" /> </Box>) : ""}
     {value === "pune" ? pune.map((e) => <Box key={e.id}> <h1 > {e.grayText}</h1> <h1 > {e.blueText}</h1> <img src={e.img} alt="" /> </Box>) : ""}
     {value === "delhi" ? delhi.map((e) => <Box key={e.id}> <h1 > {e.grayText}</h1> <h1 > {e.blueText}</h1> <img src={e.img} alt="" /> </Box>) : ""} */}
-  {/* {value=="mumbai" ?mumbai.map((e) => (
-          <Mumbai
-            key={e.id}
-            price={e.grayText}
-            name={e.blueText}
-            image={e.img}
-            imgThumb1={e.imgThumb1}
-            imgThumb2={e.imgThumb2}
-            imgThumb3={e.imgThumb3}
-            imgThumb4={e.imgThumb4}
-            raing={e.rating}
-            ratingText={e.ratingText}
-            population={e.blueText}
-            location={e.pc__html}
-            persuasion__item={e.persuasion__item}
-            cancellation={e.cancellation}
-            pernight={e.font10}
-            tax={e.tax}
-            key={e.id}
-            country={e.grayText}
-            population={e.blueText}
-            image={e.img}
-          />
-        )):"ok"}  */}
+    {value === "mumbai" ? mumbai.map((e) => (
+      <Mumbai
+        key={e.id}
+        price={e.grayText}
+        dscprice={e.discprice}
+        name={e.blueText}
+        image={e.img}
+        imgThumb1={e.imgThumb1}
+        imgThumb2={e.imgThumb2}
+        imgThumb3={e.imgThumb3}
+        imgThumb4={e.imgThumb4}
+        rating={e.rating}
+        ratingText={e.ratingText}
+        location={e.pc__html}
+        location2={e.wordBreak}
+        item={e.persuasion__item}
+        cancellation={e.cancellation}
+        freecancellation={e.freecanc}
+        pernight={e.font10}
+        tax={e.tax}
+        offer={e.offer}
+      />
+    )) : ""}
+     {value === "pune" ? pune.map((e) => (
+      <Mumbai
+        key={e.id}
+        price={e.grayText}
+        dscprice={e.discprice}
+        name={e.blueText}
+        image={e.img}
+        imgThumb1={e.imgThumb1}
+        imgThumb2={e.imgThumb2}
+        imgThumb3={e.imgThumb3}
+        imgThumb4={e.imgThumb4}
+        rating={e.rating}
+        ratingText={e.ratingText}
+        location={e.pc__html}
+        location2={e.wordBreak}
+        item={e.persuasion__item}
+        cancellation={e.cancellation}
+        freecancellation={e.freecanc}
+        pernight={e.font10}
+        tax={e.tax}
+        offer={e.offer}
+      />
+    )) : ""}
+     {value === "delhi" ? delhi.map((e) => (
+      <Mumbai
+        key={e.id}
+        price={e.grayText}
+        dscprice={e.discprice}
+        name={e.blueText}
+        image={e.img}
+        imgThumb1={e.imgThumb1}
+        imgThumb2={e.imgThumb2}
+        imgThumb3={e.imgThumb3}
+        imgThumb4={e.imgThumb4}
+        rating={e.rating}
+        ratingText={e.ratingText}
+        location={e.pc__html}
+        location2={e.wordBreak}
+        item={e.persuasion__item}
+        cancellation={e.cancellation}
+        freecancellation={e.freecanc}
+        pernight={e.font10}
+        tax={e.tax}
+        offer={e.offer}
+      />
+    )) : ""}
   </>
   );
 }
