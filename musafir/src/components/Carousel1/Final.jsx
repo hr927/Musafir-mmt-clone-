43efcbaf2c2f0e311766249fba4ef14dd027b3b8
Himanshu Rana <hrana927@gmail.com>
2023-03-03 19:24:55 +0530

    Styling fixes for single hotel page and warning fixes

--- musafir/src/components/Carousel1/Final.jsx
+++ musafir/src/components/Carousel1/Final.jsx
@@ -2,17 +2,25 @@ import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import Item1 from "./Item1";
 import { productData, responsive } from "./data1";
-import style from "./Carousel1.module.css"
+import style from "./Carousel1.module.css";
 export default function Final1() {
-  const product = productData.map((item) => (
-    <Item1
-      name={item.name}
-      url={item.imageurl}
-    />
+  const product = productData.map((item, i) => (
+    <Item1 key={i} name={item.name} url={item.imageurl} />
   ));
   return (
-    <div className={style.main_div}style={{height:"60vh", width:"85%",backgroundColor:"white",  margin:"auto",marginBottom:"30px",borderRadius:"10px",padding:"10px"}}>
-     <h1>Handpicked Collections for You</h1>
+    <div
+      className={style.main_div}
+      style={{
+        height: "60vh",
+        width: "85%",
+        backgroundColor: "white",
+        margin: "auto",
+        marginBottom: "30px",
+        borderRadius: "10px",
+        padding: "10px",
+      }}
+    >
+      <h1>Handpicked Collections for You</h1>
 
       <Carousel showDots={false} responsive={responsive}>
         {product}
--- musafir/src/components/Carousel2/Final.jsx
+++ musafir/src/components/Carousel2/Final.jsx
@@ -2,17 +2,25 @@ import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import Item2 from "./Item2";
 import { productData, responsive } from "./data2";
-import style from "../Carousel1/Carousel1.module.css"
+import style from "../Carousel1/Carousel1.module.css";
 export default function Final2() {
-  const product = productData.map((item) => (
-    <Item2
-      name={item.name}
-      url={item.imageurl}
-    />
+  const product = productData.map((item, i) => (
+    <Item2 key={i} name={item.name} url={item.imageurl} />
   ));
   return (
-    <div  className={style.main_div} style={{height:"60vh", width:"85%",backgroundColor:"white",  margin:"auto",marginBottom:"30px",borderRadius:"10px",padding:"10px"}}>
-     <h1>Unlock Lesser-Known Wonders of India</h1>
+    <div
+      className={style.main_div}
+      style={{
+        height: "60vh",
+        width: "85%",
+        backgroundColor: "white",
+        margin: "auto",
+        marginBottom: "30px",
+        borderRadius: "10px",
+        padding: "10px",
+      }}
+    >
+      <h1>Unlock Lesser-Known Wonders of India</h1>
 
       <Carousel showDots={false} responsive={responsive}>
         {product}
