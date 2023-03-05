eb9fe0c75e7a39bc33f0c57ebc4fd178e810691d
Himanshu Rana <hrana927@gmail.com>
2023-03-05 09:45:03 +0530

    Login,SignUp responsive

--- musafir/src/components/Animation.jsx
+++ musafir/src/components/Animation.jsx
@@ -1,29 +1,53 @@
-import React from 'react'
-import AnimatedText from 'react-animated-text-content'
+import React from "react";
+import AnimatedText from "react-animated-text-content";
 
-const Animation = ({content,animationType,tag}) => {
+const Animation = ({ content, animationType, tag }) => {
   return (
     <AnimatedText
-  type="words" // animate words or chars
-  animation={{
-    x: '200px',
-    y: '-20px',
-    scale: 1.1,
-    ease: 'ease-in-out',
-  }}
-  animationType={animationType}
-  interval={0.06}
-  duration={0.8}
-  tag={tag}
-  className="animated-paragraph"
-  includeWhiteSpaces
-  threshold={0.1}
-  rootMargin="20%"
-  backgroundColor="black"
->
-  {content}
-</AnimatedText>
-  )
-}
+      type="words" // animate words or chars
+      animation={{
+        x: "200px",
+        y: "-20px",
+        scale: 1.1,
+        ease: "ease-in-out",
+      }}
+      animationType={animationType}
+      interval={0.06}
+      duration={0.8}
+      tag={tag}
+      className="animated-paragraph"
+      includeWhiteSpaces
+      threshold={0.1}
+      rootMargin="20%"
+      backgroundColor="black"
+      style={{
+        fontSize: "14px", // Default font size for large screens
+        paadingBottom: "20px",
+        "@media (max-width: 768px)": {
+          // For tablets
+          fontSize: "20px",
+          animation: {
+            x: "150px",
+            y: "-15px",
+            scale: 1,
+            ease: "ease-in-out",
+          },
+        },
+        "@media (max-width: 480px)": {
+          // For mobiles
+          fontSize: "10px",
+          animation: {
+            x: "100px",
+            y: "-10px",
+            scale: 0.9,
+            ease: "ease-in-out",
+          },
+        },
+      }}
+    >
+      {content}
+    </AnimatedText>
+  );
+};
 
-export default React.memo(Animation)
+export default Animation;
--- musafir/src/components/smallComponents/ComponentA.jsx
+++ musafir/src/components/smallComponents/ComponentA.jsx
@@ -3,61 +3,67 @@ import { Box } from "@mui/system";
 import React from "react";
 import Page1 from "./Page1";
 import SignUp from "./SignUp";
-import Animation from "../Animation"
-const ComponentA = ({
-    value,
-    props,
-    handleChange,
-    handleEmail,
-    setEmail,
-    SignInWithGoogle,
-  }) => {
-    const P1 = React.useCallback(Page1, []);
-    const P2 = React.useCallback(SignUp, []);
-  
-    return (
-      <>
-        <Box sx={{ width: "100%" }}>
-          <Tabs
-            value={value}
-            onChange={handleChange}
-            textColor="primary"
-            backgroundColor="blue"
-            indicatorColor="secondary"
-            aria-label="secondary tabs example"
-            sx={{
-              "& .MuiTabs-indicator": { backgroundColor: "blue" },
-              "& .MuiTab-root": { color: "blue" },
-              "& .Mui-selected": { color: "blue" },
-              fontSize: "500",
-            }}
-            variant="fullWidth"
-          >
-            <Tab
-              sx={{ fontSize: "15px" }}
-              value="one"
-              label="PERSONAL ACCOUNT"
-            ></Tab>
-            <Tab sx={{ fontSize: "15px" }} value="two" label="SIGN UP" />
-          </Tabs>
-          <Box
-            backgroundColor="
-  #eaf5ff"
-          >
-            <Animation {...props} />
-          </Box>
-        </Box>
-  
-        {value === "one" ? (
-          <P1
-            setEmail={setEmail}
-            handleEmail={handleEmail}
-            SignInWithGoogle={SignInWithGoogle}
-          />
-        ) : null}
-        {value === "two" ? <P2 /> : null}
-      </>
-    );
-  };
+import Animation from "../Animation";
 
-    export default React.memo(ComponentA)
+const ComponentA = ({
+  value,
+  props,
+  handleChange,
+  handleEmail,
+  setEmail,
+  SignInWithGoogle,
+}) => {
+  const P1 = React.useCallback(Page1, []);
+  const P2 = React.useCallback(SignUp, []);
+
+  return (
+    <Box sx={{ width: "100%" }}>
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        textColor="primary"
+        backgroundColor="blue"
+        indicatorColor="secondary"
+        aria-label="secondary tabs example"
+        sx={{
+          "& .MuiTabs-indicator": { backgroundColor: "blue" },
+          "& .MuiTab-root": { color: "blue" },
+          "& .Mui-selected": { color: "blue" },
+          fontSize: { xs: "14px", md: "16px", lg: "18px" },
+        }}
+        variant="fullWidth"
+        centered
+      >
+        <Tab
+          sx={{ fontSize: { xs: "12px", md: "14px", lg: "16px" } }}
+          value="one"
+          label="PERSONAL ACCOUNT"
+        />
+        <Tab
+          sx={{ fontSize: { xs: "12px", md: "14px", lg: "16px" } }}
+          value="two"
+          label="SIGN UP"
+        />
+      </Tabs>
+      <Box
+        backgroundColor="#eaf5ff"
+        sx={{ display: { xs: "none", md: "block" } }}
+      >
+        <Animation {...props} />
+      </Box>
+      <Box sx={{ display: { xs: "block", md: "none" } }}>
+        <Animation {...props} />
+      </Box>
+      {value === "one" ? (
+        <P1
+          setEmail={setEmail}
+          handleEmail={handleEmail}
+          SignInWithGoogle={SignInWithGoogle}
+        />
+      ) : null}
+      {value === "two" ? <P2 /> : null}
+    </Box>
+  );
+};
+
+export default ComponentA;
